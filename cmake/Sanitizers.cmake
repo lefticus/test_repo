678550aa5fee04fbfa52e1f9cd89b450a3592729
Template Janitor <template.janitor@example.com>
2023-06-09 16:23:19 +0000

    Cleanup template and initialize repository

--- cmake/Sanitizers.cmake
+++ cmake/Sanitizers.cmake
@@ -1,5 +1,5 @@
 function(
-  myproject_enable_sanitizers
+  test_repo_enable_sanitizers
   project_name
   ENABLE_SANITIZER_ADDRESS
   ENABLE_SANITIZER_LEAK
--- src/sample_library/sample_library.cpp
+++ src/sample_library/sample_library.cpp
@@ -1,4 +1,4 @@
-#include <myproject/sample_library.hpp>
+#include <test_repo/sample_library.hpp>
 
 int factorial(int input) noexcept
 {
